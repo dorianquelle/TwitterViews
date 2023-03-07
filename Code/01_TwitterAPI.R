dc97147839a09e3de761810bec6128c1d4f20b01
dorianquelle <38698800+dorianquelle@users.noreply.github.com>
2023-03-07 16:25:35 +0100

    Clean Data & Start API Calls

--- Code/01_TwitterAPI.R
+++ Code/01_TwitterAPI.R
@@ -1,37 +1,52 @@
 # Load libraries
-install.packages("academictwitteR")
+# Print Current Working Directory
+# set Working Directory to Code
 
+install.packages("academictwitteR")
 library(academictwitteR)
 library(stringr)
 library(openxlsx)
 
-# Load Data # # # # #
-#   Still Missing   #
-#                   #
-# # # # # # # # # # #
+# Load "../Data/Clean_MediaRatingTwitter.csv"
+media_accs <- read.csv("../Data/Clean_MediaRatingTwitter.csv")
+# Remove "@" from Twitter handles
+media_accs$Twitter.Handle <- str_replace_all(media_accs$Twitter.Handle, "@", "")
+media_accs$Twitter.ID <- NA
 
-# Get Twitter IDs of media accounts
-twitter_ids <- get_user_id(
-  media_accs$Twitter.Handle,
-  bearer_token = get_bearer(),
-  all = FALSE,
-  keep_na = TRUE
-)
+# Sequentially get Twitter IDs for each media account
+for(i in 1:nrow(media_accs)){
+  tryCatch({
+    handle <- media_accs$Twitter.Handle[i]
+    id <- get_user_id(handle, bearer_token = get_bearer())
+    # Print which handle is being processed
+    print(paste("Processing", handle))
+    media_accs$Twitter.ID[i] <- id
+  }, error = function(e) {
+    print(paste("Error for", handle))
+    print("Error",e)
+  })
+}
 
-twitter_ids <- data.frame(twitter_ids)
-twitter_ids$handle <- rownames(twitter_ids)
+for(i in 1:nrow(media_accs)){
+  handle <- media_accs$Twitter.Handle[i]
+  id <- media_accs$Twitter.ID[i]
 
-for(i in 1:nrow(twitter_ids)){
-  handle <- twitter_ids$handle[i]
-  id <- twitter_ids$twitter_ids[i]
+  # Download all Tweets for each media account Between 2022-11-01 and 2023-03-08
+  print(paste0("Downloading Tweets for ", handle, " with ID ", id, ".", " Account: ", i, "/", nrow(media_accs)))
+  print(paste("Start Time:", Sys.time()))
 
-  tweets <-
-    get_user_timeline(
-      x = id,
-      start_tweets = "2022-11-01T10:00:00Z",
-      end_tweets = "2023-02-08T10:00:00Z",
-      data_path = paste0("../Data/",User,"/")
-      n = Inf,
-    )
+  # Check whether "../Data/{handle}/" exists
+  if(!dir.exists(paste0("../Data/",handle,"/"))){
+    tweets <- get_user_timeline(
+                x = id,
+                start_tweets = "2022-11-01T00:00:00Z",
+                end_tweets = "2023-03-08T00:00:00Z",
+                data_path = paste0("../Data/",handle,"/"),
+                n = Inf
+              )
+  } else {
+     print(paste0("Directory for ", handle, " already exists."))
+     print(paste0("Skipping ", handle, "."))
+  }
 }
 
