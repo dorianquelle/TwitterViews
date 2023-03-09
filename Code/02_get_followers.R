cec017352fa79ec3a9bd7cad4f5d217cea594b46
dorianquelle <38698800+dorianquelle@users.noreply.github.com>
2023-03-09 21:27:23 +0100

    Update

--- Code/02_get_followers.R
+++ Code/02_get_followers.R
@@ -1,26 +1,78 @@
+j = commandArgs(trailingOnly=TRUE)
+# if j is list take first element
+if (is.list(j)) {
+    j = as.numeric(j[[1]])
+}else{
+    j = as.numeric(j)
+}
+j = j[2]
+
 library(tidyverse)
 library(lubridate)
 library(academictwitteR)
 library(parallel)
+library(foreach)
+library(doParallel)
+
+# import all functions from 02_util_get_followers.R
+setwd("/Users/dorianquelle/Desktop/zurich/TwitterViews/Code")
+source("02_util_get_followers.R")
 
 # Set up API Tokens
 # Read tokens.txt
 tokens <- readLines("../tokens.txt")
+# Ignore lines that start with #
+tokens <- tokens[!grepl("^#", tokens)]
+# check for duplicates
+if (length(tokens) != length(unique(tokens))){
+    print("Duplicate tokens found!")
+    print("Removing duplicates...")
+    # Print prior and post length
+    print(paste("Prior length:", length(tokens)))
+    tokens <- unique(tokens)
+    print(paste("Post length:", length(tokens)))
+}
+
+#token_valid <- rep(FALSE, length(tokens))
+## check if token works 
+#for(i in 1:length(tokens)){
+#    tryCatch({
+#        a <- get_user_followers(x = "1336014519160758278", bearer_token = tokens[i], verbose = FALSE)
+#        token_valid[i] <- TRUE
+#        cat("\r", i, "/", length(tokens), " tokens valid")
+#    }, error = function(e){
+#        tokens_valid[i] <- FALSE
+#        cat("\r", i, "/", length(tokens), " tokens NOT valid")
+#    })
+#}
+## Remove invalid tokens (Print Prior and Post length)
+#print("Removing invalid tokens...")
+print(paste("Prior length:", length(tokens)))
+#tokens <- tokens[token_valid]
+#print(paste("Post length:", length(tokens)))
+tokens <- tokens[seq(j, length(tokens), 5)]
+print(paste("Post length:", length(tokens)))
+
 # Get followers of Media Accounts
-media_accs <- read.csv("../Mediadatabases/Clean_MediaRatingTwitter.csv")
-media_accs$Twitter.Handle <- str_replace_all(media_accs$Twitter.Handle, "@", "")
+media_accs <- read.csv("../Mediadatabases/small_Clean_MediaRatingTwitter.csv")
+media_accs <- media_accs[seq(j, nrow(media_accs), 5), ]
 
+# Filter media_accs to only include accounts with Twitter.ID
+# Create 5 groups of tokens
+# Each group will be used by a worker
+token_grouped <- split(tokens, 1:length(tokens) %% 5)
 
-get_followers <- function(id, handle, token){
+get_followers <- function(handle, tokens){
+    id <- get_user_id(handle, bearer_token = tokens[1])
         # Print random number to check if function is running
-        print(paste("Start Time:", Sys.time(), "for", handle, runif(1, 0, 100)))
+        print(paste("Start Time:", Sys.time(), "for", handle))
         tryCatch({
             # Check if directory exists 
             # If it does, skip
             if(!dir.exists(paste0("../Followers/Media/",handle,"/"))){ 
-                followers <- get_user_followers(
+                followers <- get_user_followers_pimped(
                             x = id,
-                            bearer_token = token
+                            bearer_token = tokens
                             )
 
                 # Add Twitter Handle and Twitter ID to followers
@@ -48,38 +100,65 @@ get_followers <- function(id, handle, token){
         return 
 }
 
+for(i in 1:nrow(media_accs)){
+    get_followers(media_accs$Twitter.Handle[i], tokens)
+}
+
 
 # Spawn a pool of n workers
 # Where n is the number of tokens that we have
 # Get the number of tokens
-n_token <- length(tokens)
-# Create Test Run
-
-# Create a list of arguments for each worker
-args <- lapply(1:nrow(media_accs), function(i){
-    list(id = media_accs$Twitter.ID[i],
-         handle = media_accs$Twitter.Handle[i])
-})
-
-# Split args into n_token subsets
-args_list <- split(args, rep(1:n_token, length.out = length(args)))
-for(i in 1:n_token){
-    for(j in 1:length(args_list[[i]])){
-        args_list[[i]][[j]]$token <- tokens[i]
-    }
-}
+#n_token <- length(token_grouped)
+## Create Test Run
+#
+## Create a list of arguments for each worker
+#args <- lapply(1:nrow(media_accs), function(i){
+#    list(handle = media_accs$Twitter.Handle[i])
+#})
+#
+## Split args into n_token subsets
+#args_list <- split(args, rep(1:n_token, length.out = length(args)))
+#for(i in 1:n_token){
+#    for(j in 1:length(args_list[[i]])){
+#        args_list[[i]][[j]]$token <- unlist(token_grouped[i])
+#    }
+#}
 
 # Define a function to run get_followers on each element of args
-run_get_followers <- function(arg) {
-  get_followers(arg[[1]]$id, arg[[1]]$handle, arg[[1]]$token)
-}
+#run_get_followers <- function(arg) {
+#    # assert that id is a character, handle is a character, and token is a list
+#    tryCatch({
+#          get_followers(arg[[1]]$handle, arg[[1]]$token)
+#    }, error = function(e){
+#        print(paste("Error in run_get_followers", arg[[1]]$handle,"at", Sys.time()))
+#        print(paste("Error",e))
+#        # Print current time
+#    })
+#}
 
-# Create a cluster with three nodes
-cl <- makeCluster(n_token, outfile = "/Users/dorianquelle/Desktop/zurich/TwitterViews/Code/MultiProcessingLog.txt")
-clusterExport(cl, c("run_get_followers", "get_followers","get_user_followers"))
-# Run run_get_followers on each subset of args in parallel
-results <- parLapplyLB(cl = cl, args_list, run_get_followers)
+# Set up a parallel backend
+#cl <- makeCluster(c("localhost", "localhost", "localhost","localhost","localhost"), type = "PSOCK",outfile = paste0(getwd(),"/MultiProcessingLog.txt"))
+#clusterExport(cl, c("run_get_followers", "get_followers","get_user_followers","get_user_followers_pimped","get_user_edges_pimped","get_user_id", "make_query",".check_header_rate_limit","add_context_annotations", "add_query_prefix", ".process_qparam", ".trigger_sleep", ".check_reset",".vwarn",".vcat",".gen_random_dir"))
+#clusterApply(cl, args_list, run_get_followers)
+#
+## Close the cluster
+#stopCluster(cl)
 
-# Stop the cluster
-stopCluster(cl)
+
+
+
+#media_accs$Twitter.Handle <- str_replace_all(media_accs$Twitter.Handle, "@", "")
+#
+##Check if Twitter.ID is in column names
+#if(!"Twitter.ID" %in% colnames(media_accs)){
+#    media_accs$Twitter.ID <- NA
+#    for(i in 1:nrow(media_accs)){
+#        if(is.na(media_accs[i, "Twitter.ID"])){
+#            media_accs[i, "Twitter.ID"] <- get_user_id(media_accs[i, "Twitter.Handle"])
+#        }
+#    cat("\r", i, "/", nrow(media_accs), " Twitter.IDs added")
+#    }
+#}
+## save the file
+#write.csv(media_accs, "../Mediadatabases/small_Clean_MediaRatingTwitter.csv", row.names = FALSE)
 
---
+++ Code/02_util_get_followers.R
@@ -0,0 +1,189 @@
+get_user_followers_pimped <- function(x, bearer_tokens){
+  get_user_edges_pimped(x = x, wt = "followers", bearer_tokens)
+}
+
+get_user_edges_pimped <- function(x, bearer_tokens, wt, verbose = TRUE){  
+  url <- "https://api.twitter.com/2/users/"
+  
+endpoint <- "/followers"
+params <- list(
+    "max_results" = 1000,
+    "user.fields" = "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld"
+)
+  
+  new_df <- data.frame()
+  for(i in seq_along(x)){
+    cat(paste0("Processing ",x[i],"\n"))
+    count_pages <- 0
+    next_token <- ""
+    while (!is.null(next_token)) {
+      count_pages <- count_pages + 1
+      requrl <- paste0(url,x[i],endpoint)
+      
+      if(next_token!=""){
+        params[["pagination_token"]] <- next_token
+      }
+      c_bearer_token = bearer_tokens[count_pages %% length(bearer_tokens) + 1]
+
+      # Try Catch the query, If error remove the current token. 
+      tryCatch({
+        dat <- make_query(url = requrl, params = params, bearer_token = c_bearer_token, verbose = verbose)   
+      }, error = function(e){
+        if(length(bearer_tokens) > 1){
+          bearer_tokens = bearer_tokens[-which(bearer_tokens == c_bearer_token)]
+        } else {
+          print("No more tokens to try. Exiting.")
+        }
+      })
+
+      next_token <- dat$meta$next_token #this is NULL if there are no pages left
+      new_rows <- dat$data
+      new_rows$from_id <- x[i]
+      new_df <- dplyr::bind_rows(new_df, new_rows) # add new rows
+      
+      cat("Total data points: ",nrow(new_df), "\n")
+      Sys.sleep(1)
+      if (is.null(next_token)) {
+        if(verbose) {
+          cat("This is the last page for ",
+              x[i],
+              ": finishing collection. \n")
+        }
+        break
+      }
+      Sys.sleep(1)
+    }
+  }
+  return(new_df)
+}
+
+
+
+
+##### UTILS
+
+make_query <- function(url, params, bearer_token, max_error = 4, verbose = TRUE) {
+  bearer_token <- paste0("Bearer ",bearer_token)
+  count <- 0
+  while (TRUE) {
+    if (count >= max_error) {
+      stop("Too many errors.")
+    }
+    pre_time <- Sys.time()
+    r <- httr::GET(url, httr::add_headers(Authorization = bearer_token), query = params)
+    time_diff <- as.numeric(Sys.time() - pre_time)
+    if (time_diff < 1) { ## To prevent #231
+      Sys.sleep(1)
+    }
+    status_code <- httr::status_code(r)
+    if (!status_code %in% c(200, 429, 503)) {
+      stop(paste("something went wrong. Status code:", httr::status_code(r)))
+    }
+    if (.check_header_rate_limit(r, verbose = verbose)) {
+      count <- count + 1
+    }
+    if (status_code == 200) {
+      break()
+    }
+    if (status_code == 503) {
+      count <- count + 1
+      Sys.sleep(count * 5)
+    }
+    if (status_code == 429) {
+      .trigger_sleep(r, verbose = verbose)
+      count <- count + 1
+    }
+  }
+  jsonlite::fromJSON(httr::content(r, "text"))
+}
+
+
+.check_header_rate_limit <- function(r, verbose) {
+  if (is.null(httr::headers(r)$`x-rate-limit-remaining`)) {
+    return(FALSE)
+  }
+  if (httr::headers(r)$`x-rate-limit-remaining` == "1") {
+    .vwarn(verbose, paste("x-rate-limit-remaining=1. Resets at", .check_reset(r)))
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
+.gen_random_dir <- function() {
+  file.path(tempdir(), paste0(sample(letters, 20), collapse = ""))
+}
+
+
+.vcat <- function(bool, ...) {
+  if (bool) {
+    cat(...)
+  }
+}
+
+.vwarn <- function(bool, ...) {
+  if (bool) {
+    warning(..., call. = FALSE)
+  }
+}
+
+.check_reset <- function(r, tzone = "") {
+  lubridate::with_tz(lubridate::as_datetime(as.numeric(httr::headers(r)$`x-rate-limit-reset`), tz = tzone), tzone)
+}
+
+
+.trigger_sleep <- function(r, verbose = TRUE, really_sleep = TRUE, ref_time = Sys.time(), tzone = "") {
+  reset_time <- .check_reset(r, tzone = tzone)
+  ## add 1s as buffer
+  sleep_period <- ceiling(as.numeric(reset_time - ref_time, units = "secs")) + 1
+  if (sleep_period < 0) {
+    ## issue #213
+    .vcat(verbose, "Rate limit reached. Cannot estimate adaptive sleep time. Sleeping for 900 seconds. \n")
+    sleep_period <- 900
+  } else {
+    .vcat(verbose, "Rate limit reached. Rate limit will reset at", as.character(reset_time) ,"\nSleeping for", sleep_period ,"seconds. \n")
+  }
+  if (verbose) {
+    pb <- utils::txtProgressBar(min = 0, max = sleep_period, initial = 0)
+    for (i in seq_len(sleep_period)) {
+      utils::setTxtProgressBar(pb, i)
+      if (really_sleep) {
+        Sys.sleep(1)
+      }
+    }
+  } else {
+    if (really_sleep) {
+      Sys.sleep(sleep_period)
+    }
+  }
+  invisible(r)
+}
+
+
+.process_qparam <- function(param, param_str,query) {
+  if(!is.null(param)){
+    if(isTRUE(param)) {
+      query <- paste(query, param_str)
+    } else if(param == FALSE) {
+      query <- paste(query, paste0("-", param_str))
+    }
+  }
+  return(query)
+}
+
+
+add_query_prefix <- function(x, prefix){
+  q <- paste0(prefix, x)
+  q <- paste(q, collapse = " OR ")
+  q <- paste0("(",q,")")
+  return(q)
+}
+
+add_context_annotations <- function(params, verbose){
+  if(params[["max_results"]] > 100){
+    params[["max_results"]] <- 100
+    .vcat(verbose, "page_n is limited to 100 due to the restriction imposed by Twitter API\n")
+  }
+  params[["tweet.fields"]] <- "attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,source,text,withheld"
+  params
+}
